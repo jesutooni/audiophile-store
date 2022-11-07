d9076b7de3cbbd251b18b580b2e8a98e3107cc00
Jesutooni <edeworuyoyou@gmail.com>
2022-11-07 11:47:40 +0100

    update button component

--- src/components/global/button.jsx
+++ src/components/global/button.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { ReactComponent as RightCaret } from "../../assets/images/icons/caret-right.svg";
 
-const button = ({ theme, children }) => {
+const button = ({ theme, text }) => {
   const themeClasses = {
     peach: "bg-peach-dark hover:bg-peach-light text-white",
     white:
       "bg-white hover:bg-black border border-black text-black hover:text-white",
-    transparent: "text-black opacity-50 hover:opacity-100 text-peach-dark",
+    transparent:
+      "text-primary-black-50 hover:opacity-100 hover:text-peach-dark",
   };
   return (
-    <button className={`py-4 px-8 font-bold text-sm ${themeClasses[theme]}`}>
-      {children}
+    <button
+      className={`py-4 px-8 font-bold text-sm uppercase flex items-center gap-3 ${themeClasses[theme]} duration-150`}
+    >
+      {text}
+      <RightCaret />
     </button>
   );
 };
