ea02b70c01b10024c06a01e16be227f8986e1aaa
Jesutooni <edeworuyoyou@gmail.com>
2022-11-08 12:37:34 +0100

    responsiveness for header footer

--- src/components/base/footer.jsx
+++ src/components/base/footer.jsx
@@ -4,38 +4,51 @@ import facebook from "../../assets/images/icons/facebook.svg";
 import twitter from "../../assets/images/icons/twitter.svg";
 import instagram from "../../assets/images/icons/instagram.svg";
 
+const SocialIcons = ({ customClass }) => {
+  return (
+    <div
+      className={`${customClass} items-center justify-center gap-4`}
+    >
+      <img src={facebook} alt="facebook-logo" className="cursor-pointer" />
+      <img src={twitter} alt="twitter-logo" className="cursor-pointer" />
+      <img src={instagram} alt="instagram-logo" className="cursor-pointer" />
+    </div>
+  );
+};
+
 const footer = () => {
   const navs = ["Home", "Headphones", "Speakers", "Earphones"];
   return (
-    <footer className="bg-primary-black pt-20 pb-12">
-      <div className="max-w-6xl mx-auto flex justify-between">
-        <div>
+    <footer className="bg-primary-black pt-14 sm:pt-20 pb-12">
+      <div className="max-w-6xl mx-auto px-6 flex flex-col items-center sm:items-start space-y-12">
+        <div className="flex flex-col md:flex-row items-center sm:items-start justify-between w-full gap-12">
           <Logo />
-          <p className="mt-9 max-w-[540px] font-medium text-[15px] leading-6 text-white opacity-50">
+          <ul className="text-white flex flex-col sm:flex-row items-center gap-4 sm:gap-8 uppercase text-sm font-bold">
+            {navs.map((nav) => (
+              <li
+                key={nav}
+                className="cursor-pointer hover:text-peach-dark duration-200 tracking-[2px]"
+              >
+                {nav}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex justify-between w-full">
+          <p className="md:max-w-[540px] font-medium text-[15px] leading-6 text-primary-white-50 text-center sm:text-left">
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we're open 7 days a week.
           </p>
+          <SocialIcons customClass="hidden md:flex self-end" />
+        </div>
 
-          <p className="mt-14 font-bold text-white opacity-50 text-sm">
+        <div className="flex justify-between flex-col sm:flex-row w-full gap-12">
+          <p className="font-bold text-primary-white-50 text-sm text-center">
             Copyright 2021. All Rights Reserved
           </p>
-        </div>
-        <div>
-          <ul className="text-white flex items-center gap-8 uppercase text-sm font-bold">
-            {navs.map((nav) => (
-              <li key={nav} className="cursor-pointer">
-                {nav}
-              </li>
-            ))}
-          </ul>
-
-          <div className="mt-24 flex items-center justify-end gap-4">
-            <img src={facebook} alt="facebook-logo" />
-            <img src={twitter} alt="twitter-logo" />
-            <img src={instagram} alt="instagram-logo" />
-          </div>
+          <SocialIcons customClass="flex md:hidden" />
         </div>
       </div>
     </footer>
--- src/components/base/header.jsx
+++ src/components/base/header.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../assets/images/audiophile.svg";
+import { ReactComponent as Hamburger } from "../../assets/images/icons/hamburger.svg";
 import CartIcon from "../../assets/images/icons/cart.svg";
 
 const navigation = () => {
   const navs = ["Home", "Headphones", "Speakers", "Earphones"];
   return (
     <header className="bg-black">
-      <nav className="flex items-center justify-between  py-9 border-b border-white max-w-6xl mx-auto">
-        <Logo />
-        <ul className="text-white flex items-center gap-8 uppercase text-sm font-bold">
-          {navs.map((nav) => (
-            <li key={nav} className="cursor-pointer">
-              {nav}
-            </li>
-          ))}
-        </ul>
-        <div>
-          <img src={CartIcon} alt="cart-icon" />
-        </div>
-      </nav>
+      <div className="px-6">
+        <nav className="flex items-center justify-between py-9 border-b border-white max-w-6xl mx-auto">
+          <div className="flex gap-10 items-center">
+            <Hamburger className="md:hidden cursor-pointer" />
+            <Logo className="hidden sm:block cursor-pointer" />
+          </div>
+          <Logo className="sm:hidden"/>
+          <ul className="text-white hidden md:flex items-center gap-8 uppercase text-sm font-bold">
+            {navs.map((nav) => (
+              <li
+                key={nav}
+                className="cursor-pointer hover:text-peach-dark duration-200 tracking-[2px]"
+              >
+                {nav}
+              </li>
+            ))}
+          </ul>
+          <div>
+            <img src={CartIcon} alt="cart-icon" />
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
