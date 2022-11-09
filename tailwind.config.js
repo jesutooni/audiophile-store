839c1b2d869839c4e9c836cb7d40a79ee3344ea6
Jesutooni <edeworuyoyou@gmail.com>
2022-11-09 16:24:49 +0100

    update custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
           black: "#101010",
           'black-50': '#00000080',
           'white-50': '#FFFFFF80',
+          'white-75': '#FFFFFFBF',
           white1: "#F1F1F1",
           white2: "#FAFAFA",
         },
