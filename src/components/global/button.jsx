d797c1bbc1468c552b02956b2228b5c423bfeae4
Jesutooni <edeworuyoyou@gmail.com>
2022-11-09 16:31:29 +0100

    update button component

--- src/components/global/button.jsx
+++ src/components/global/button.jsx
@@ -6,12 +6,13 @@ const button = ({ theme, text }) => {
     peach: "bg-peach-dark hover:bg-peach-light text-white",
     white:
       "bg-white hover:bg-black border border-black text-black hover:text-white",
+    black: "bg-black hover:bg-[#4C4C4C] text-white",
     transparent:
       "text-primary-black-50 hover:opacity-100 hover:text-peach-dark",
   };
   return (
     <button
-      className={`py-3.5 px-8 font-bold text-xs uppercase flex items-center gap-3 ${themeClasses[theme]} duration-150`}
+      className={`py-3.5 px-8 font-bold text-xs uppercase flex items-center gap-3 ${themeClasses[theme]} duration-300 ease-in-out`}
     >
       {text}
       {theme === "transparent" && <RightCaret />}
